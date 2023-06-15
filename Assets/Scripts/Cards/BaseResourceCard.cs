342f23d0435914434026241e8a805480d56083de
Nikos P <nikos.petrop37@gmail.com>
2023-06-16 00:29:45 +0300

    +Card animation on selection

--- Assets/Scripts/Cards/BaseResourceCard.cs
+++ Assets/Scripts/Cards/BaseResourceCard.cs
@@ -1,22 +1,31 @@
+using System;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class BaseResourceCard : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler, IPointerDownHandler, IPointerUpHandler, IDragHandler {
+    
+    
     public int CurrentDurability;
     public int MaxDurability;
     public ResourceType Resource;
+    public PlayerHand playerHand;
 
     public RectTransform Transform { get; private set; }
+    
+    [SerializeField] private Animation dotweenAnimation;
 
     private Vector3 startingPosition;
     private Quaternion startingRotation;
     private bool isDragging;
     private Vector2 pointerOffset;
     private Vector2 originalLocalPointerPosition;
+    private int startingIndex;
 
 
     private void Awake() {
         Transform = GetComponent<RectTransform>();
+        playerHand = FindObjectOfType<PlayerHand>();
     }
 
     public void Initialize() {
@@ -25,9 +34,22 @@ public class BaseResourceCard : MonoBehaviour, IPointerEnterHandler, IPointerExi
 
     private void Start() { }
 
-    public virtual void OnPointerEnter(PointerEventData eventData) { }
+    public virtual void OnPointerEnter(PointerEventData eventData) {
+        startingIndex = transform.GetSiblingIndex();
+        transform.SetAsLastSibling();
+        var sequence = DOTween.Sequence();
+        sequence.Append(Transform.DOScale(dotweenAnimation.Scale, dotweenAnimation.Duration));
+        sequence.Join(Transform.DOLocalMoveY(dotweenAnimation.Height, dotweenAnimation.Duration));
+        sequence.Join(Transform.DOLocalRotate(Vector3.zero, dotweenAnimation.Duration));
+    }
 
-    public virtual void OnPointerExit(PointerEventData eventData) { }
+    public virtual void OnPointerExit(PointerEventData eventData) {
+        transform.SetSiblingIndex(startingIndex);
+        var sequence = DOTween.Sequence();
+        sequence.Append(Transform.DOScale(1f, dotweenAnimation.Duration));
+        sequence.Join(Transform.DOLocalMove(startingPosition, dotweenAnimation.Duration));
+        sequence.Join(Transform.DOLocalRotate(startingRotation.eulerAngles, dotweenAnimation.Duration));
+    }
 
     public virtual void OnPointerClick(PointerEventData eventData) {
         if (eventData.button == PointerEventData.InputButton.Right) {
@@ -52,6 +74,13 @@ public class BaseResourceCard : MonoBehaviour, IPointerEnterHandler, IPointerExi
         if (isDragging) {
             Transform.position = Input.mousePosition;
         }
+    }
+
+    [Serializable]
+    private class Animation {
+        public float Scale;
+        public float Height;
+        public float Duration;
     }
 
     public enum ResourceType {
